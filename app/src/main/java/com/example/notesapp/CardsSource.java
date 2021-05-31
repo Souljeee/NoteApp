c203080d9abfc28818eef2eb9836315bbff2ac31
Souljeee <drobitek76106055@gmail.com>
2021-05-31 16:41:20 +0300

    Homework for lesson9

--- app/src/main/java/com/example/notesapp/CardsSource.java
+++ app/src/main/java/com/example/notesapp/CardsSource.java
@@ -1,6 +1,40 @@
 package com.example.notesapp;
 
-public interface CardsSource {
-    Note getNote(int position);
-    int size();
+import android.content.res.Resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardsSource {
+    private static List<Note> dataSource = new ArrayList<>();
+    private Resources resources;
+
+
+    public static Note getNote(int position) {
+        return dataSource.get(position);
+    }
+
+    public static int size() {
+        return dataSource.size();
+    }
+
+
+    public static void deleteNote(int position) {
+        dataSource.remove(position);
+    }
+
+
+    public static void updateNote(int position, Note note) {
+        dataSource.set(position, note);
+    }
+
+
+    public static void addNote(Note note) {
+        dataSource.add(note);
+    }
+
+
+    public static void clearNote() {
+        dataSource.clear();
+    }
 }
